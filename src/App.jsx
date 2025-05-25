343f2caf7fc39bf384832ada7b9a90ff5e529865
Sarika Kushwaha <sarika.vsa01@gmail.com>
2025-05-25 18:26:14 +0530

    fix: resolved toast visibility issue by increasing z-index on .Toastify container

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import "./app.css";
 
+
+
 // const [isModalOpen, setIsModalOpen] = useState(false);
 
 // const handleOpenModal = () => setIsModalOpen(true);
@@ -23,28 +25,55 @@ import "./app.css";
 function App() {
   const [showContact, setShowContact] = useState(false);
   return (
-    <>
-      <div>
 
-        <div className="main-app-container">
-          {/* <Navbar /> */}
-          <Navbar onContactClick={() => setShowContact(true)} />
-          <Hero onContactClick={() => setShowContact(true)} />
-        </div>
+    <div>
+      <style>
+  {`
+    .Toastify {
+      padding: 0 !important;
+      min-height: 0 !important;
+      z-index: 9999 !important;
+    }
 
-        <About />
-        <Education />
-        <Experience />
-        <Projects />
-        <Skills />
-        <Certificate />
-        <Footer />
+    @media (max-width: 768px) {
+      .Toastify {
+        padding: 0 !important;
+        min-height: 0 !important;
+        
+      }
+    }
+
+    @media (max-width: 480px) {
+      .Toastify {
+        padding: 0 !important;
+        min-height: 0 !important;
+        
+      }
+    }
+  `}
+</style>
 
 
-        <ContactOverlay show={showContact} onClose={() => setShowContact(false)} />
+
+      <div className="main-app-container">
+        {/* <Navbar /> */}
+        <Navbar onContactClick={() => setShowContact(true)} />
+        <Hero onContactClick={() => setShowContact(true)} />
       </div>
+
+      <About />
+      <Education />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Certificate />
+      <Footer />
+
+
+      <ContactOverlay show={showContact} onClose={() => setShowContact(false)} />
       <ToastContainer position="top-center" autoClose={5000} pauseOnHover />
-    </>
+    </div>
+
   );
 }
 
